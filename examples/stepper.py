d0b68012eab64cadbb439ce3b056695eb8ac3fc5
nabeelfkhan <nubzero@gmail.com>
2016-12-23 01:00:07 -0800

    v2

--- examples/stepper.py
+++ examples/stepper.py
@@ -1,17 +1,29 @@
-# Pulsates an LED connected to GPIO pin 1 with a suitable resistor 4 times using softPwm
-# softPwm uses a fixed frequency
+#Code to interface with DRV8821 and two stepper motors
+
 import wiringpi
 
-OUTPUT = 1
 
-PWMA = 7
+#Pin number listing
+
+PWMA = 7 #Uses actual pin numbers, not GPIO number
 PWMB = 13
-HIGH = 1 
+ABRST = 33
+ABDIR = 35
+CDRST = 37
+CDDIR = 15
+
+# Static Variables
+OUTPUT = 1
+HIGH = 1
 LOW = 0
 
 wiringpi.wiringPiSetup()
 wiringpi.pinMode(PWMA,OUTPUT)
 wiringpi.pinMode(PWMB,OUTPUT)
+wiringpi.pinMode(ABRST,OUTPUT)
+wiringpi.pinMode(ABDIR,OUTPUT)
+wiringpi.pinMode(CDRST,OUTPUT)
+wiringpi.pinMode(CDDIR,OUTPUT)
 
 #for time in range(0,4):
 #	for brightness in range(0,100): # Going from 0 to 100 will give us full off to full on
@@ -24,6 +36,14 @@ wiringpi.pinMode(PWMB,OUTPUT)
 
 pwmacount= int(raw_input("Enter number of PWM pulses for pin 7:\n"))
 
+#Inputs for later
+MotorABDir = 1
+MotorCDDir = 0
+
+wiringpi.digitalWrite(ABRST,HIGH) #Enable motor 1
+wiringpi.digitalWrite(CDRST,HIGH) #Enable motor 2
+wiringpi.digitalWrite(ABDIR,MotorABDir) #Enable motor 2
+wiringpi.digitalWrite(CDDIR,MotorCDDir) #Enable motor 2
 
 
 
@@ -33,5 +53,11 @@ for pulses in range(0,pwmacount):
 	wiringpi.delayMicroseconds(500) #f = 900Hz (pulses per second). T = 1/f. Delay = T/2 = 555.56us> Modified based on scope
 	wiringpi.digitalWrite(PWMA,LOW)
 	wiringpi.delayMicroseconds(500)
+	
+	#wiringpi.digitalWrite(PWMB,HIGH)
+	#wiringpi.delayMicroseconds(500) #f = 900Hz (pulses per second). T = 1/f. Delay = T/2 = 555.56us> Modified based on scope
+	#wiringpi.digitalWrite(PWMB,LOW)
+	#wiringpi.delayMicroseconds(500)
 
-
+wiringpi.digitalWrite(ABRST,LOW) #Disable motor 1
+wiringpi.digitalWrite(CDRST,LOW) #Disable motor 2
